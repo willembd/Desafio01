4386efff3e4c8ce5eaf54f3ae03dfbec5e92c761
willemberg bruno <willem.bruno@hotmail.com>
2025-04-05 21:57:28 -0300

    Desafio 01

---
+++ desafio01.py
@@ -0,0 +1,21 @@
+h1 = int(input("Digite a hora: "))
+m1 = int(input("Digite os minutos: "))
+h2 = int(input("Digite a hora: "))
+m2 = int(input("Digite os minutos: "))
+
+somah = h1 + h2
+somam = m1 + m2
+
+if somah > 12 and somah < 24:
+    somah -=12
+else:
+    if somah > 24:
+        somah -=24
+    if somah > 12:
+        somah -=12
+
+if somam > 60:
+    somah +=1
+    somam -=60
+
+print(f"{somah} Horas e {somam} Minutos")
